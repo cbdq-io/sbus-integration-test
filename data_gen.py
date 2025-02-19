e31202b41358c72989cee3d49559c02d117113d0
Ben Dalling <b.dalling@cbdq.io>
2025-02-19 18:47:20 +0000

    feat: Release 0.1.0.

--- data_gen.py
+++ data_gen.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python
 """A script to inject data into Kafka."""
 import argparse
+import json
 import logging
 import os
 import sys
+
+from random import choice
+from string import ascii_uppercase
 
 import lorem
 from kafka import KafkaProducer
 from kafka.errors import KafkaError
 
-DEFAULT_MESSAGE_COUNT = 10_000
+DEFAULT_MESSAGE_COUNT = 128_000
 DEFAULT_TOPIC_COUNT = 10
+
+MESSAGE_SIZES = [
+    1024,
+    4500,
+    4800,
+    5000,
+    4116
+]
+
 PROG_NAME = os.path.basename(__file__)
 
 logging.basicConfig()
@@ -36,12 +49,27 @@ logger.info(
 
 bootstrap_servers = ['localhost:9092']
 producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+last_reported_percentage = 0
 
 for i in range(DEFAULT_MESSAGE_COUNT):
     topic_number = i % DEFAULT_TOPIC_COUNT
     topic_name = f'topic.{topic_number}'
-    message = lorem.sentence().encode()
-    future = producer.send(topic_name, message)
+    message_size = MESSAGE_SIZES[i % 5]
+    record = {
+        'message_number': i,
+        'topic_name': topic_name,
+        'text': lorem.sentence(),
+        'payload': ''
+    }
+    json_record = json.dumps(record)
+    payload_size = message_size - len(json_record)
+    payload = ''.join(
+        choice(ascii_uppercase) for i in range(payload_size)  # nosec B311
+    )
+    record['payload'] = payload
+    message = json.dumps(record)
+    logger.debug(f'{len(message)} - {message}')
+    future = producer.send(topic_name, message.encode())
 
     try:
         record_metadata = future.get(timeout=10)
@@ -52,3 +80,9 @@ for i in range(DEFAULT_MESSAGE_COUNT):
     except KafkaError:
         logger.error("Failed to send message", exc_info=True)
         sys.exit(1)
+
+    percentage_complete = round((i / DEFAULT_MESSAGE_COUNT) * 100, 0)
+
+    if percentage_complete != last_reported_percentage:
+        logger.info(f'Processed {i:,} messages of {DEFAULT_MESSAGE_COUNT} ({percentage_complete}%).')
+        last_reported_percentage = percentage_complete
